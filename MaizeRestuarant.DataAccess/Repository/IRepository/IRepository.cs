44cbf99fa16de7795561461ab4f8b055cdeb8826
LungileFuze <ngcobolungile94@outlook.com>
2023-06-17 14:19:09 +0200

    Implementing Reposatory

--- MaizeRestuarant.DataAccess/Repository/IRepository/IRepository.cs
+++ MaizeRestuarant.DataAccess/Repository/IRepository/IRepository.cs
@@ -5,13 +5,15 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MaizeRestuarant.DataAccess.Repository
+namespace MaizeRestuarant.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
         //Get All, Get by Id First or Default, Add, Remove, RemoveRange
         void Add(T entity);
         void Delete(T entity);
+
+        void RemoveRange(IEnumerable<T> entity);
         IEnumerable<T> GetAll();
         T GetFirstorDefault(Expression<Func<T, bool>>? filter = null);
     }
---
+++ MaizeRestuarant.DataAccess/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using MaizeRestuarant.DataAccess.Data;
+using MaizeRestuarant.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaizeRestuarant.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly MaizeRestuarantDbContext _context;
+        internal DbSet<T> dbSet;
+
+        public Repository(MaizeRestuarantDbContext context)
+        {
+            _context = context;
+            this.dbSet = _context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();  
+        }
+
+        public T GetFirstorDefault(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return query.FirstOrDefault(filter);
+        }
+    }
+}
