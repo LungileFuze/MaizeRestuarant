6f773177ba4a6ff2af11c5e92cf444bf689c8e08
LungileFuze <ngcobolungile94@outlook.com>
2023-06-17 13:54:33 +0200

    Create IReposatory

---
+++ MaizeRestuarant.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaizeRestuarant.DataAccess.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        //Get All, Get by Id First or Default, Add, Remove, RemoveRange
+        void Add(T entity);
+        void Delete(T entity);
+        IEnumerable<T> GetAll();
+        T GetFirstorDefault(Expression<Func<T, bool>>? filter = null);
+    }
+}
