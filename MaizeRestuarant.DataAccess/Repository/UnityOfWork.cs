180efd98699b81a11c2cc7b4d79525b6ad047576
LungileFuze <ngcobolungile94@outlook.com>
2023-06-18 11:46:30 +0200

    Unity of work implementation

---
+++ MaizeRestuarant.DataAccess/Repository/UnityOfWork.cs
@@ -0,0 +1,31 @@
+﻿using MaizeRestuarant.DataAccess.Data;
+using MaizeRestuarant.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaizeRestuarant.DataAccess.Repository
+{
+    public class UnityOfWork : IUnityOfWork
+    {
+        private readonly MaizeRestuarantDbContext _context;     
+        public UnityOfWork(MaizeRestuarantDbContext dbContext) 
+        { 
+            _context = dbContext;
+            Category = new CategoryRepository(dbContext);
+        }
+        public ICategoryRepository Category { get; private set; }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
