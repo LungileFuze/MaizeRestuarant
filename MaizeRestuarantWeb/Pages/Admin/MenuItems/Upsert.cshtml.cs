edf19be2346089537a3df1ab89f9ff7d19c9fb73
LungileFuze <ngcobolungile94@outlook.com>
2023-06-24 15:59:54 +0200

    Create Menu item Post Handler

--- MaizeRestuarantWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
+++ MaizeRestuarantWeb/Pages/Admin/MenuItems/Upsert.cshtml.cs
@@ -3,6 +3,7 @@ using MaizeRestuarant.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using static System.Net.WebRequestMethods;
 
 namespace MaizeRestuarantWeb.Pages.Admin.MenuItems
 {
@@ -10,6 +11,7 @@ namespace MaizeRestuarantWeb.Pages.Admin.MenuItems
     public class UpsertModel : PageModel
     {
         private readonly IUnityOfWork _unityOfWork;
+        private readonly IWebHostEnvironment _webEnvironment;
 
         public MenuItem MenuItem { get; set; }  
 
@@ -17,10 +19,11 @@ namespace MaizeRestuarantWeb.Pages.Admin.MenuItems
 
         public IEnumerable<SelectListItem>FoodTypeList { get; set; }
 
-        public UpsertModel(IUnityOfWork unityOfWork)
+        public UpsertModel(IUnityOfWork unityOfWork, IWebHostEnvironment webEnvironment)
         {
             _unityOfWork = unityOfWork;
-            MenuItem = new();
+            _webEnvironment = webEnvironment;
+            MenuItem = new(); 
         }
         public void OnGet()
         {
@@ -36,10 +39,30 @@ namespace MaizeRestuarantWeb.Pages.Admin.MenuItems
                 Value = f.Id.ToString(),    
             });
         }
-
+       
         public async Task<IActionResult> OnPost() 
         {
-            return Page();
+            string webRootPath = _webEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+            if(MenuItem.Id == 0) 
+            { 
+                string fileName_new = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(webRootPath, @"images\menuItems");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var fileStream = new FileStream(Path.Combine(uploads, fileName_new + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+                MenuItem.Image = @"\images\menuItems\" + fileName_new + extension;
+                _unityOfWork.MenuItem.Add(MenuItem);
+                _unityOfWork.Save();
+            }
+            else
+            {
+                
+            }
+            return RedirectToPage("./Index");
         }
     }
 }
